ea190b352f772076882f00f1de840af9c804acc5
Yan <bbb.zzzz.bbb@mail.ru>
2022-04-26 16:33:57 +0200

    simple pig lating

---
+++ simple_pig_latin.py
@@ -0,0 +1,19 @@
+'''
+Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+Leave punctuation marks untouched.
+'''
+
+def pig_it(text):
+    full = []
+    for word in text.split(' '):
+        if word.isalpha():
+            right_list = []
+            right_list.append([word[letter] for letter in range(1, len(word))])
+            right_list[0].append(word[0])
+            right_list[0].append('ay')
+            right_str = "".join(right_list[0])
+            full.append(right_str)
+        else:
+            full.append('!')
+    print(" ".join(full))
+    return " ".join(full)
